972c06f83c150d410099abf7770789049bf71329
KelseyEtcheverry <48496980+KelseyEtcheverry@users.noreply.github.com>
2020-04-20 15:00:38 -0600

    Create EdsbyStudentSchoolEnrollment.ps1 (#2)
    Added the SQL Query for the StudentSchoolEnrollment.csv.

---
+++ Edsby/EdsbyStudentSchoolEnrollment.ps1
@@ -0,0 +1,65 @@
+param (
+    [Parameter(Mandatory=$true)][string]$OutputFileName,
+    [string]$ConfigFilePath = "$(Split-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) -Parent)/config.xml"
+ )
+
+##############################################
+# Script configuration                       #
+##############################################
+
+# SQL Query to run
+# The output CSV file will use column names from your SQL query.
+# Rename them using "as" - example: "SELECT cFirstName as FirstName FROM Students"
+$SqlQuery = "SELECT 
+                S.iStudentID AS StudentGUID, 
+                ss.iSchoolID AS SchoolID 
+            FROM Student S
+            INNER JOIN StudentStatus SS
+                ON S.iStudentID = SS.iStudentID
+            WHERE (SS.dInDate <=  { fn CURDATE() }) 
+                AND ((SS.dOutDate < '1901-01-01') 
+                OR (SS.dOutDate >=  { fn CURDATE() }))
+            ORDER BY S.iStudentID;"
+
+# CSV Delimeter
+# Some systems expect this to be a tab "\t" or a pipe "|".
+$Delimeter = ','
+
+# Should all columns be quoted, or just those that contains characters to escape?
+# Note: This has no effect on systems with PowerShell versions <7.0 (all fields will be quoted otherwise)
+$QuoteAllColumns = $false
+
+##############################################
+# No configurable settings beyond this point #
+##############################################
+
+# Retreive the connection string from config.xml
+if ((test-path -Path $ConfigFilePath) -eq $false) {
+    Throw "Config file not found. Specify using -ConfigFilePath. Defaults to config.xml in the directory above where this script is run from."
+}
+$configXML = [xml](Get-Content $ConfigFilePath)
+$ConnectionString = $configXML.Settings.SchoolLogic.ConnectionString
+
+# Set up the SQL connection
+$SqlConnection = new-object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = $ConnectionString
+$SqlCommand = New-Object System.Data.SqlClient.SqlCommand
+$SqlCommand.CommandText = $SqlQuery
+$SqlCommand.Connection = $SqlConnection
+$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+$SqlAdapter.SelectCommand = $SqlCommand
+$SqlDataSet = New-Object System.Data.DataSet
+
+# Run the SQL query
+$SqlConnection.open()
+$SqlAdapter.Fill($SqlDataSet)
+$SqlConnection.close()
+
+# Output to a CSV file
+foreach($DSTable in $SqlDataSet.Tables) {
+    if (($PSVersionTable.PSVersion.Major -ge 7) -and ($QuoteAllColumns -eq $false)) {
+        $DSTable | export-csv $OutputFileName -notypeinformation -Delimiter $Delimeter -UseQuotes AsNeeded
+    } else {        
+        $DSTable | export-csv $OutputFileName -notypeinformation -Delimiter $Delimeter
+    }
+}
