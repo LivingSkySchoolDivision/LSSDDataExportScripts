ce3269e27c40c798e11c6c57943b6fb95e3207b8
Kelsey Etcheverry <kelsey.etcheverry@lskysd.ca>
2020-04-22 11:14:52 -0600

    Create EdsbyStaffSectionAssignment.ps1
    Added the SQL query for the StaffSectionAssignment

---
+++ Edsby/EdsbyStaffSectionAssignment.ps1
@@ -0,0 +1,71 @@
+param (
+    [Parameter(Mandatory=$true)][string]$OutputFileName,
+    [string]$ConfigFilePath
+ )
+
+##############################################
+# Script configuration                       #
+##############################################
+
+# SQL Query to run
+# The output CSV file will use column names from your SQL query.
+# Rename them using "as" - example: "SELECT cFirstName as FirstName FROM Students"
+$SqlQuery = "SELECT
+                C.iSchoolID AS SchoolID,
+                CONCAT(C.iSchoolID,'-',C.iClassID) AS SectionGUID,
+                CONCAT('STAFF-',CR.iStaffID) AS StaffGUID,
+                R.cName AS Role
+            FROM Class C
+                LEFT OUTER JOIN ClassResource CR ON C.iClassID = CR.iClassID
+                LEFT OUTER JOIN UserStaff US ON CR.iStaffID = US.iStaffID
+                LEFT OUTER JOIN LookupValues R ON US.iEdsbyRoleid = R.iLookupValuesID
+            ORDER BY C.iSchoolID, CR.iStaffID"
+
+# CSV Delimeter
+# Some systems expect this to be a tab "\t" or a pipe "|".
+$Delimeter = ','
+
+# Should all columns be quoted, or just those that contains characters to escape?
+# Note: This has no effect on systems with PowerShell versions <7.0 (all fields will be quoted otherwise)
+$QuoteAllColumns = $false
+
+##############################################
+# No configurable settings beyond this point #
+##############################################
+
+# Find the config file
+$AdjustedConfigFilePath = $ConfigFilePath
+if ($AdjustedConfigFilePath.Length -le 0) {
+    $AdjustedConfigFilePath = join-path -Path $(Split-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) -Parent) -ChildPath "config.xml"
+}
+
+# Retreive the connection string from config.xml
+if ((test-path -Path $AdjustedConfigFilePath) -eq $false) {
+    Throw "Config file not found. Specify using -ConfigFilePath. Defaults to config.xml in the directory above where this script is run from."
+}
+$configXML = [xml](Get-Content $AdjustedConfigFilePath)
+$ConnectionString = $configXML.Settings.SchoolLogic.ConnectionString
+
+# Set up the SQL connection
+$SqlConnection = new-object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = $ConnectionString
+$SqlCommand = New-Object System.Data.SqlClient.SqlCommand
+$SqlCommand.CommandText = $SqlQuery
+$SqlCommand.Connection = $SqlConnection
+$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+$SqlAdapter.SelectCommand = $SqlCommand
+$SqlDataSet = New-Object System.Data.DataSet
+
+# Run the SQL query
+$SqlConnection.open()
+$SqlAdapter.Fill($SqlDataSet)
+$SqlConnection.close()
+
+# Output to a CSV file
+foreach($DSTable in $SqlDataSet.Tables) {
+    if (($PSVersionTable.PSVersion.Major -ge 7) -and ($QuoteAllColumns -eq $false)) {
+        $DSTable | export-csv $OutputFileName -notypeinformation -Delimiter $Delimeter -UseQuotes AsNeeded
+    } else {        
+        $DSTable | export-csv $OutputFileName -notypeinformation -Delimiter $Delimeter
+    }
+}
