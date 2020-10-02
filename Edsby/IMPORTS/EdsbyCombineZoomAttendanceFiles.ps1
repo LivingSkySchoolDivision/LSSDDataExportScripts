f7e6de694ad689cbe6cf649b4208cb9fdf25c71e
Mark Strendin <mark.strendin@lskysd.ca>
2020-10-02 08:45:39 -0600

    Add script to combine attendance files into a single file

---
+++ Edsby/IMPORTS/EdsbyCombineZoomAttendanceFiles.ps1
@@ -0,0 +1,44 @@
+param (
+    [Parameter(Mandatory=$true)][string]$EdsbyLinkOutDirectory,
+    [Parameter(Mandatory=$true)][string]$OutputFileName,
+    [string]$ConfigFilePath
+ )
+
+function Get-CSV {
+    param(
+        [Parameter(Mandatory=$true)][String] $CSVFile
+    )
+
+    return import-csv $CSVFile -header('SchoolID','IncidentID','IncidentDate','UpdateDate','StudentFirstName','StudentLastName','StudentGUID','StudentID','StudentMinistryID','StudentGrade','PeriodIDs','MeetingID','MeetingStartTime','MeetingEndTime','Class','ClassGUID','TeacherNames','TeacherGUIDs','Room','Code','ReasonCode','Reason','Comment','Tags') | Select -skip 2
+}
+
+# CSV Delimeter
+# Some systems expect this to be a tab "`t" or a pipe "|".
+$Delimeter = ','
+
+# Check if the output file exists, and if it does, delete it
+if (Test-Path $OutputFileName) 
+{
+    Remove-Item $OutputFileName
+}
+
+$CurrentLocation = Get-Location
+$fullOutputFilePath = Join-Path $CurrentLocation $OutputFileName
+
+write-host "Reading all csv files in `"$EdsbyLinkOutDirectory`""
+Set-Location $EdsbyLinkOutDirectory
+
+Get-ChildItem .\*.csv | ForEach-Object {           
+    $fileOutputRows = @()
+
+    Write-Output "Processing $($_.Name)..."     
+    $inputrecords = @(Get-CSV -CSVFile $_.Name)    
+    foreach($record in $inputrecords)
+    {    
+        $fileOutputRows += $record                
+    }            
+
+    $fileOutputRows | ForEach-Object {[PSCustomObject]$_} | export-csv $fullOutputFilePath -notypeinformation -Delimiter $Delimeter
+}
+Set-Location $CurrentLocation
+
