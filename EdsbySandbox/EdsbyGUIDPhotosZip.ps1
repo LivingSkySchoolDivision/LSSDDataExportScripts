f008f478463327f7e9b820898ebadcc5d10e9d77
KelseyEtcheverry <48496980+KelseyEtcheverry@users.noreply.github.com>
2021-03-09 13:56:36 -0600

    Hard coded path for ActiveSchools (#151)
    Updated the location of the ActiveSchools.csv

--- EdsbySandbox/EdsbyGUIDPhotosZip.ps1
+++ EdsbySandbox/EdsbyGUIDPhotosZip.ps1
@@ -10,7 +10,7 @@ param (
 
 # Edsby allows us to sync roughly a dozen schools to the Sandbox
 # The ActiveSchools.csv can be used to adjust the schools we want to send
-$ActiveSchools = Import-Csv -Path .\.\ActiveSchools.csv | Where-Object {$_.Sync -eq 'T'} 
+$ActiveSchools = Import-Csv -Path C:\Sync\EdsbySandbox\ActiveSchools.csv | Where-Object {$_.Sync -eq 'T'} 
 $iSchoolIDs = Foreach ($ID in $ActiveSchools) {
     if ($ID -ne $ActiveSchools[-1]) { $ID.iSchoolID + ',' } else { $ID.iSchoolID }    
 }
