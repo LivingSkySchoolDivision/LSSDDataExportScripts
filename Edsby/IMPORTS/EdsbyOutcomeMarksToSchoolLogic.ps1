fad3471d870b0024a74f1303b932439a36d34111
Mark Strendin <mark.strendin@gmail.com>
2021-03-16 16:28:21 -0600

    Script now detects missing outcomes a bit more intelligently

--- Edsby/IMPORTS/EdsbyOutcomeMarksToSchoolLogic.ps1
+++ Edsby/IMPORTS/EdsbyOutcomeMarksToSchoolLogic.ps1
@@ -1,10 +1,11 @@
 param (
     [Parameter(Mandatory=$true)][string]$InputFileName,
     [string]$ConfigFilePath,
+    [switch]$ImportUnknownOutcomes,
     [switch]$DryRun
  )
 
- ###########################################################################
+###########################################################################
 # Functions                                                               #
 ###########################################################################
 
@@ -34,6 +35,22 @@ function Validate-CSV {
     if ($line.Contains('"SectionGUID"') -eq $false) { throw "Input CSV missing field: SectionGUID" }    
     return $true
 }
+Function Get-Hash
+{
+    param
+    (
+        [String] $String
+    )
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($String)
+    $hashfunction = [System.Security.Cryptography.HashAlgorithm]::Create('SHA1')
+    $StringBuilder = New-Object System.Text.StringBuilder
+    $hashfunction.ComputeHash($bytes) |
+    ForEach-Object {
+        $null = $StringBuilder.Append($_.ToString("x2"))
+    }
+
+    return $StringBuilder.ToString()
+}
 
 function Get-CSV {
     param(
@@ -44,6 +61,42 @@ function Get-CSV {
         return import-csv $CSVFile  | Select -skip 1
     } else {
         throw "CSV file is not valid - cannot continue"
+    }
+}
+
+function Convert-ToSLOutcomeMark {
+    param(
+        [Parameter(Mandatory=$true)] $InputRow
+    )
+    # Parse cMark vs nMark
+    $cMark = ""
+    $nMark = ""
+
+    if ([bool]($InputRow.Grade -as [decimal]) -eq $true) {
+        $nMark = [decimal]$InputRow.Grade
+        if (
+            ($nMark -eq 1) -or
+            ($nMark -eq 1.5) -or
+            ($nMark -eq 2) -or
+            ($nMark -eq 2.5) -or
+            ($nMark -eq 3) -or
+            ($nMark -eq 3.5) -or
+            ($nMark -eq 4)
+        ) {
+            $cMark = [string]$nMark
+        }
+    } else {
+        $cMark = $InputRow.Grade
+    }
+
+    return [PSCustomObject]@{
+        iStudentID = [int](Convert-StudentID $InputRow.StudentGUID)
+        iReportPeriodID = [int]0
+        iCourseObjectiveId = [int](Convert-ObjectiveID -OutcomeCode $InputRow.CriterionName -Objectives $SLCourseObjectives -iCourseID $InputRow.CourseCode)
+        iCourseID = [int]$InputRow.CourseCode
+        iSchoolID = [int]$InputRow.SchoolID
+        nMark = [decimal]$nMark
+        cMark = [string]$cMark
     }
 }
 
@@ -82,12 +135,34 @@ function Get-SQLData {
     return $null
 }
 
+function Convert-ObjectiveID {
+    param(
+        [Parameter(Mandatory=$true)] $OutcomeCode,
+        [Parameter(Mandatory=$true)] $iCourseID,
+        [Parameter(Mandatory=$true)] $Objectives
+    )
+    foreach($obj in $Objectives) {
+        if (($obj.OutcomeCode -eq $OutcomeCode) -and ($obj.iCourseID -eq $iCourseID))
+        {
+            return $obj.iCourseObjectiveID
+        }
+    } 
+
+    return -1
+}
+
 ###########################################################################
 # Script initialization                                                   #
 ###########################################################################
 
 if ($DryRun -eq $true) {
     Write-Log "Performing dry run - will not actually commit changes to the database"
+}
+
+if ($ImportUnknownOutcomes -eq $true) {
+    Write-Log "When encountering a mark for an outcome that doesn't exist in SchoolLogic, script will: Import those outcomes into SchoolLogic"
+} else {
+    Write-Log "When encountering a mark for an outcome that doesn't exist in SchoolLogic, script will: Ignore those marks"
 }
 
 Write-Log "Loading config file..."
@@ -128,12 +203,43 @@ Write-Log "Loading and validating input CSV file..."
 $CSVInputFile = Get-CSV -CSVFile $InputFileName
 
 ###########################################################################
+# Collect required info from the SL database                              #
+###########################################################################
+
+Write-Log "Loading required data from SchoolLogic DB..."
+
+$SQLQuery_CourseObjectives = "SELECT iCourseObjectiveID, OutcomeCode, iCourseID, cSubject FROM CourseObjective"
+
+# Convert to hashtables for easier consumption
+$SLCourseObjectives_Raw = Get-SQLData -ConnectionString $DBConnectionString -SQLQuery $SQLQuery_CourseObjectives
+Write-Log " Loaded $($SLCourseObjectives_Raw.Length) course objectives."
+
+# Put course objectives in a hashtable for easy lookups
+$SLCourseObjectives = @()
+
+foreach($Obj in $SLCourseObjectives_Raw) {
+    if (($Obj.OutcomeCode -ne "") -and ($null -ne $Obj.OutcomeCode)) {
+            $Outcome = [PSCustomObject]@{
+            OutcomeCode = $Obj.OutcomeCode
+            OutcomeText = $Obj.OutcomeText
+            iCourseObjectiveID = $Obj.iCourseObjectiveID
+            iCourseID = $Obj.iCourseID
+            cSubject = $Obj.cSubject
+        }
+        $SLCourseObjectives += $Outcome
+        
+    }
+}
+
+###########################################################################
 # Process the file                                                        #
 ###########################################################################
 
 Write-Log "Processing input file..."
 Write-Log " Rows to process: $($CSVInputFile.Length)"
 $OutcomeMarksToImport = @()
+$OutcomeMarksNeedingOutcomes = @()
+$OutcomeNotFound = @{}
 
 foreach ($InputRow in $CSVInputFile)
 {
@@ -142,44 +248,37 @@ foreach ($InputRow in $CSVInputFile)
         continue;
     }
 
-    # Parse cMark vs nMark
-    $cMark = ""
-    $nMark = ""
+    # Assemble the final mark object
+    $NewOutcomeMark = Convert-ToSLOutcomeMark -InputRow $InputRow
 
-    if ([bool]($InputRow.Grade -as [decimal]) -eq $true) {
-        $nMark = [decimal]$InputRow.Grade
-        if (
-            ($nMark -eq 1) -or
-            ($nMark -eq 1.5) -or
-            ($nMark -eq 2) -or
-            ($nMark -eq 2.5) -or
-            ($nMark -eq 3) -or
-            ($nMark -eq 3.5) -or
-            ($nMark -eq 4)
-        ) {
-            $cMark = [string]$nMark
+    if ($NewOutcomeMark.iCourseObjectiveId -eq -1) {
+        $OutcomeMarksNeedingOutcomes += $InputRow
+        $Fingerprint = (Get-Hash -String ("$($InputRow.CourseCode)$($InputRow.CriterionName)"))
+        if ($OutcomeNotFound.ContainsKey($Fingerprint) -eq $false) {
+            $OutcomeNotFound.Add($Fingerprint,[PSCustomObject]@{
+                iCourseID = [int]$InputRow.CourseCode
+                OutcomeCode = [string]$InputRow.CriterionName
+                OutcomeText = [string]$InputRow.CriterionDesc
+                cSubject = "$($InputRow.CriterionName) $($InputRow.CriterionDesc)"
+                mNotes = "$($InputRow.CriterionName) $($InputRow.CriterionDesc)"
+                iLV_ObjectiveCategoryID = 4146
+            })
         }
     } else {
-        $cMark = $InputRow.Grade
+        $OutcomeMarksToImport += ($NewOutcomeMark)
     }
-
-    # Assemble the final mark object
-    $NewOutcomeMark = [PSCustomObject]@{
-        iStudentID = [int](Convert-StudentID $InputRow.StudentGUID)
-        iReportPeriodID = [int]0
-        iCourseObjectiveId = [int]0 # Look up from objective hashtable
-        iCourseID = [int]0 # Lookup from course list
-        iSchoolID = [int]$InputRow.SchoolID
-        nMark = [decimal]$nMark
-        cMark = [string]$cMark
-    }
-
-    $OutcomeMarksToImport += ($NewOutcomeMark)
 }
 
+Write-Log "Found $($OutcomeMarksToImport.Length) marks to import"
+Write-Log "Found $($OutcomeMarksNeedingOutcomes.Length) without matching outcomes in SchoolLogic"
+Write-Log "Found $($($OutcomeNotFound.Count)) outcomes that don't exist in our database."
 
-Write-Log "[DEBUG] Outputting marks to console..."
-foreach($OM in $OutcomeMarksToImport) {
-    Write-Log $OM
+if ($ImportUnknownOutcomes -eq $true) {
+# Insert new outcomes that didn't exist in SL before
+
+# Re-import outcomes from SchoolLogic
+
+# Reprocess marks that didn't have matching outcomes before
+} else {
+    Write-Log "Skipping $($OutcomeMarksNeedingOutcomes.Length) marks due to missing outcomes."
 }
-Write-Log "Total marks to import: $($OutcomeMarksToImport.Length)"
